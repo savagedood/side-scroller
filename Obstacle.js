a7d19097e80a364d669113b8ae985f58fee29588
savagedood <76217053+savagedood@users.noreply.github.com>
2021-01-14 13:03:26 -0500

    Obstacle.js
    updated obstacle file

--- Obstacle.js
+++ Obstacle.js
@@ -1,20 +1,35 @@
-class obstacle {
-  constructor(){
-    this.pos = createVector(random(width), random(height-10));
+class Obstacle {
+  constructor() {
+    this.pos = createVector(random(man.pos.x + 40, man.pos.x + 1200), random(height - 11));
     this.vel = createVector(-0.25, 0);
-    this.acc = createVector(0,0);
+    this.acc = createVector(0, 0);
+    this.quality = good;
   }
-  
-  show(){
-    fill(random(255),random(255),random(255));
-    rect(this.pos.x,this.pos.y, 10,10)
-        fill(random(255),random(255),random(255));
-    ellipse(this.pos.x,this.pos.y, 10,10)
+
+  show() {
+    noStroke();
+    fill(random(255));
+    // changing these numbers makes the collision not work, why?
+    rect(this.pos.x, this.pos.y, 10, 10) // try changing the numbers in person collision too
+    
   }
-  
-  update(){
+
+  update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
+  }
+}
+
+class Special extends Obstacle {
+  constructor() {
+    super();
+     this.quality = star;
+  }
+
+  show() {
+    fill(255, 165, 0);
+    ellipse(this.pos.x, this.pos.y, 20, 20)
+    
   }
 }
