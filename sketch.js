45285158195fe94f1115932d8d49ca494c868a79
savagedood <76217053+savagedood@users.noreply.github.com>
2021-01-15 09:13:26 -0500

    sketch.js
    adding updated sketch

--- sketch.js
+++ sketch.js
@@ -1,96 +1,58 @@
+
+function scene0() {
+  background("blue");var timerValue = 15;
+var good = 1;
+var star = 0;
+var landscape;
+var fuel;
 var man;
 var bad = [];
-var sceneNum = 0;
-//finish up hit function https://www.youtube.com/watch?v=l0HoJHc-63Q&t=1027s
+var bonus = [];
+var currentScene = 1;
+var endSceneCountdown = 0;
+
 function setup() {
   createCanvas(640, 360);
   man = new Person();
-  for (let i = 0; i < 100; i++) {
-    bad[i] = new obstacle();
+  drawScene1();
+  for (let i = 0; i < 150; i++) {
+    bad[i] = new Obstacle();
   }
+  for (let j = 0; j < 35; j++) {
+    bonus[j] = new Special();
+  }
+  setInterval(timeIt, 1000);
 
 }
-
-function keyPressed() {
-  if (key == ' ') {
-    let force = createVector(0, -5);
-    man.applyForce(force);
-  } else if (key == 's') {
-    sceneNum++;
-
-  }
-
-  if (sceneNum > 2) {
-    sceneNum = 0;
-  }
+// scene 1, the start scene
+var drawScene1 = function() {
+  currentScene = 1;
+  man.score = 0;
+  background(0, 0, 255);
+  textSize(50);
+  textAlign(CENTER);
+  fill(0);
+  text("Asteroid collector!!!", width / 2, 50);
+  textSize(30);
+  fill("red")
+  text("Click to start mission", width / 2, 100);
+  noStroke();
+  fill("red");
+  rect(width / 4, height / 2, 50, 50);
+  fill(255);
+  rect(width / 2, height * 3 / 4, 60, 60);
 }
 
-function draw() {
-  if (sceneNum === 0) {
-    scene0();
-
-  } else if (sceneNum === 1) {
-    scene1();
-  } else if (sceneNum === 2) {
-    scene2();
-  }
-
-
-}
-
-function scene1() {
-  background(51);
-  translate(-man.pos.x + 100, 0);
-  let gravity = createVector(0, 0.05)
-  man.applyForce(gravity);
-  man.update();
-  man.display();
-  man.edges();
-  //Silly hard code or obstacle
-  fill(50, 30, 200);
-  rect(200, height - 50, 100, 100);
-
-  for (let i = 0; i < 100; i++) {
-    if (man.hits(bad[i])) {
-      console.log("Game Over!");
-      console.log(bad[i].pos.x);
-    }
-    //console.log(bad[i].pos.x);
-
-    bad[i].show();
-    bad[i].update();
-  }
-
-}
-
-function scene2() {
-  background(5,1,2);
-  translate(-man.pos.x + 100, 0);
-  let gravity = createVector(0, 0.5)
-  man.applyForce(gravity);
-  man.update();
-  man.display();
-  man.edges();
-  //Silly hard code or obstacle
-  fill(50, 30, 200);
-  rect(200, height - 50, 100, 100);
-
-  for (let i = 0; i < 100; i++) {
-    if (man.hits(bad[i])) {
-      console.log("Game Over!");
-      console.log(bad[i].pos.x);
-    }
-    //console.log(bad[i].pos.x);
-    bad[i].acc.x=-0.01;
-
-    bad[i].show();
-    bad[i].update();
-  }
-
-}
-
-function scene0() {
-  background("blue");
+var drawScene2 = function() {
+  currentScene = 2;
+  man.score = 0;
+  background(0, 0, 255);
+  textSize(50);
+  textAlign(CENTER);
+  fill("white");
+  text("The Task", width / 2, 50);
+  textSize(20);
+  text("collect as many asteroid samples as you can in 15 seconds", width / 2, 75);
   textSize(50);
   fill(255,0,0);
   text("space asteroids!!!", 100, 100);
