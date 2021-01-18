01d6233b42fa13238d576a31768eb0c286bb2c16
savagedood <76217053+savagedood@users.noreply.github.com>
2021-01-18 12:27:12 -0500

    sketch.js
    updated sketch

--- sketch.js
+++ sketch.js
@@ -1,6 +1,4 @@
-
-function scene0() {
-  background("blue");var timerValue = 15;
+var timerValue = 15;
 var good = 1;
 var star = 0;
 var landscape;
@@ -53,8 +51,132 @@ var drawScene2 = function() {
   text("The Task", width / 2, 50);
   textSize(20);
   text("collect as many asteroid samples as you can in 15 seconds", width / 2, 75);
-  textSize(50);
-  fill(255,0,0);
-  text("space asteroids!!!", 100, 100);
-  text("press s key to start", 100, 200);
+  text("fly your drone over the samples to pick them up", width / 2, 100);
+  text("watchout for stars which will dicintergrate your samples", width / 2, 120);
+  text("Click to launch drone", width / 2, 165);
+  text("Asteroid ------>", width / 2, 245);
+  noStroke();
+  fill(255);
+  rect(width / 4, height / 2, 0, 50);
+  fill(255);
+  rect(width / 2 + 100, height / 2 + 25, 60, 60);
 }
+var drawScene3 = function() {
+  currentScene = 3;
+  background(space);
+  fill("red")
+  
+  if (timerValue > 10) {
+    text("0:" + timerValue, 500, 85);
+  }
+  if (timerValue <= 10) {
+    text('0:0' + timerValue, 500, 85);
+  }
+   if (timerValue ===10) {  
+    text('0:' + timerValue, 500, 85);
+  }
+  if (timerValue === 0) {
+    text('game over', width / 2, height / 2 + 15);
+  }
+ 
+  // changed this number, not sure what it does, will check it out later
+  translate(-man.pos.x + 50, 0);
+  endSceneCountdown += 1.2;
+
+  let gravity = createVector(0, 0.20);
+  man.applyForce(gravity);
+
+  man.update();
+  man.display();
+  man.edges();
+
+  for (let i = 0; i < 150; i++) {
+    if (man.hits(bad[i])) {
+      console.log(bad[i].pos.x);
+    }
+    bad[i].show();
+    bad[i].update();
+  }
+
+  for (let j = 0; j < 35; j++) {
+    if (man.hits(bonus[j])) {
+      console.log(bonus[j].pos.x);
+    }
+    bonus[j].show();
+    bonus[j].update();
+  }
+
+  if (timerValue === 0) {
+    drawScene4();
+  }
+}
+
+var drawScene4 = function() {
+  currentScene = 4;
+  endSceneCountdown = 0;
+  man.reset();
+  
+  resetMatrix();
+
+  bad = [];
+  for (let i = 0; i < 150; i++) {
+    bad[i] = new Obstacle();
+  }
+
+  bonus = [];
+  for (let j = 0; j < 35; j++) {
+    bonus[j] = new Special();
+  }
+
+  background(fuel);
+  textSize(20);
+  textAlign(CENTER);
+  noStroke();
+  fill("red");
+  text("You finished the game and returned to earth!", width / 2, 50);
+  text("Your score was " + man.score + ". Great job!", width / 2, 80);
+  text("click the play button to refuel and try again", width / 2, 300);
+}
+// changed the height of the jump
+function keyPressed() {
+  if (key == " ") {
+    let jump = createVector(0, -6);
+    man.applyForce(jump);
+  }
+}
+
+function preload() {
+  space = loadImage('download (3).jpg')
+  fuel = loadImage('fuel-gauge.jpg')
+}
+
+function draw() {
+  if (currentScene === 1) {
+    drawScene1();
+  }
+  if (currentScene === 2) {
+    drawScene2();
+  }
+  if (currentScene === 3) {
+    drawScene3();
+  }
+}
+
+function timeIt() {
+  if (timerValue > 0) {
+    timerValue--;
+  }
+
+}
+
+function mousePressed() {
+  if (currentScene === 1) {
+    drawScene2();
+  } else if (currentScene === 2) {
+    timerValue = 15;
+    drawScene3();
+  } else if (currentScene === 4) {
+    drawScene1();
+  }
+}
+
